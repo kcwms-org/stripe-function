df63e7257bc7f0da4929ffff4fc47ff1606daef9
Kevin Williams <kevcoder@gmail.com>
2023-03-09 22:53:12 -0600

    removed unneeded async on app.get

--- create-checkout-session-endpoint.js
+++ create-checkout-session-endpoint.js
@@ -1,5 +1,5 @@
 // This is your test secret API key.
-const stripeCheckout =require('./packages/stripe-checkout/create-checkout-session');
+const stripeCheckout = require('./packages/stripe-checkout/create-checkout-session');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -8,16 +8,16 @@ app.use(express.json());
 app.use(cors());
 
 const PORT_NUMBER = process.env.API_PORT
-app.get('/heart-beat', async (req,res)=>{
+app.get('/heart-beat', (req, res) => {
   res.status(200).send(new Date().toISOString());
 });
 
 app.post('/create-checkout-session', async (req, res) => {
-  
-    const payment = req.body;
-    const session = await stripeCheckout.getStripeCheckoutUrl({
+
+  const payment = req.body;
+  const session = await stripeCheckout.getStripeCheckoutUrl({
     line_items: [
-      {      
+      {
         price: payment.productId,
         quantity: payment.quantity,
       },
@@ -27,7 +27,7 @@ app.post('/create-checkout-session', async (req, res) => {
     cancel_url: `${payment.domain}${payment.failRoute}`,
   });
 
-  res.json({url: session.url});
+  res.json({ url: session.url });
 });
 
 app.listen(PORT_NUMBER, () => console.log(`Running on port ${PORT_NUMBER}`));
